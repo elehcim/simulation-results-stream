e5ff9548871836b724e6411c1eb2e616ae0989e4
Michele Mastropietro <michele.mastropietro@gmail.com>
2020-05-20 10:07:59 +0200

    Heroku files

---
+++ setup.sh
@@ -0,0 +1,15 @@
+mkdir -p ~/.streamlit/
+
+echo "\
+[general]\n\
+email = \"michele.mastropietro@gmail.com\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+" > ~/.streamlit/config.toml
+
+python -m pip install git+https://github.com/elehcim/simulation.git
